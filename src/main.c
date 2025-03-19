f7edb4db0546770a1fa35b8a103185d02e0bd7b2
Todd Cope <todd@t3-i.com>
2025-03-19 08:42:09 -0500

    Remove unused function.

--- src/main.c
+++ src/main.c
@@ -247,38 +247,6 @@ static void ocd_convert_grey_to_alpha(ALLEGRO_BITMAP *bitmap)
 	al_unlock_bitmap(bitmap);
 }
 
-static ALLEGRO_FONT * load_font(char * fn)
-{
-	int ranges[] = {32, 126};
-	ALLEGRO_FONT * fp;
-	ALLEGRO_STATE old_state;
-	ALLEGRO_BITMAP * fimage;
-	char buf[1024];
-//	int old_format = al_get_new_bitmap_format();
-
-	al_store_state(&old_state, ALLEGRO_STATE_NEW_BITMAP_PARAMETERS);
-//	al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_ANY_WITH_ALPHA);
-	al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP | ALLEGRO_NO_PREMULTIPLIED_ALPHA);
-	fimage = al_load_bitmap(fn);
-	if(!fimage)
-	{
-		return NULL;
-	}
-	ocd_convert_grey_to_alpha(fimage);
-	sprintf(buf, "%s.png", fn);
-	al_save_bitmap(buf, fimage);
-	al_restore_state(&old_state);
-	fp = al_grab_font_from_bitmap(fimage, 1, ranges);
-/*	if(!fp)
-	{
-		al_destroy_bitmap(fimage);
-		return NULL;
-	} */
-	al_destroy_bitmap(fimage);
-//	al_set_new_bitmap_format(old_format);
-	return fp;
-}
-
 static void _lingo_setup_urls(void)
 {
 	const char * val;

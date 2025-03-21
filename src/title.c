c7c90fd6cd6d16ece13acd55b6000f141dda53f8
Todd Cope <todd@t3-i.com>
2025-03-21 12:38:15 -0500

    Add 'draw_text' module for handling specialized text rendering. Add copyright message to title screen.

--- src/title.c
+++ src/title.c
@@ -6,6 +6,7 @@
 #include "main.h"
 #include "title.h"
 #include "menu_proc.h"
+#include "draw_text.h"
 
 void lingo_menu_create(LINGO_MENU * mp, char * name, int parent, int x, int y, int flags)
 {
@@ -237,4 +238,5 @@ void lingo_title_render(void * data)
 			t3f_draw_text(instance->menu[instance->current_menu].item[i].font, color, mx, y, 0, 0, text);
 		}
 	}
+	lingo_draw_text_with_shadow(instance->font[LINGO_FONT_SPRINT_10], t3f_color_white, al_map_rgba(0, 0, 0, 128), instance->view->virtual_width / 2, instance->view->virtual_height - t3f_get_font_line_height(instance->font[LINGO_FONT_SPRINT_10]) - 2, 0, 2, 2, T3F_FONT_ALIGN_CENTER, T3F_APP_COPYRIGHT);
 }
